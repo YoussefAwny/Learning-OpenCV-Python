f4a93e93c900b286ca784deb415be4afa0c7e568
youssef Awny <youssefawny@gmail.com>
2018-09-23 15:42:25 +0200

    object detection using canny

---
+++ Canny edge detection.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+img = cv2.imread("tomatoes.jpg",1)
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+res,thresh = cv2.threshold(hsv[:,:,0], 25, 255, cv2.THRESH_BINARY_INV)
+cv2.imshow("Thresh",thresh)
+
+edges = cv2.Canny(img, 100, 70)
+cv2.imshow("Canny",edges)
+
+img2 = cv2.bitwise_and(edges, thresh)
+cv2.imshow("final", img2)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
